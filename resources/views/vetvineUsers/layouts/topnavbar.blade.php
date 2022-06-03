269f263acfeea11ea41d0d82e20dd89b0ce6d5ed
fahadhussnain <fahadhussnain102@gmail.com>
2022-06-03 15:31:35 +0500

    pull

--- resources/views/vetvineUsers/layouts/topnavbar.blade.php
+++ resources/views/vetvineUsers/layouts/topnavbar.blade.php
@@ -1,84 +1,81 @@
-
-
-
-
 <div class="top-social">
 
-          <ul class="list__icons">
+    <ul class="list__icons">
 
-            <li>
+        <li>
 
-              <div class="input-group mb-3">
+            <div class="input-group mb-3">
 
                 <span class="search-text">Search: &nbsp;</span>
 
                 <input type="text" class="form-control top-search" placeholder="" aria-label="Recipient's username"
-
-                  aria-describedby="basic-addon2">
+                    aria-describedby="basic-addon2">
 
                 <div class="input-group-append">
 
-                  <button class="btn search_btn" type="button">
+                    <button class="btn search_btn" type="button">
 
-                    <i class="fa fa-search"></i>
+                        <i class="fa fa-search"></i>
 
-                  </button>
+                    </button>
 
                 </div>
 
-              </div>
+            </div>
 
-            </li>
+        </li>
 
-            <li>
+        <li>
+            <a href="#" class="toggle-contact" data-toggle="modal" data-target="#myPopup"><span class="mail-logo"><i
+                        class="fa fa-envelope"></i></span> Stay in touch</a>
+        </li>
 
-              <form method="POST" action="{{ route('logout') }}">
-            @csrf
-              <a class="reg-login_btn" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                    this.closest('form').submit();">Logout</a>
+        <li class="logout">
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <a class="toggle-contact" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                    this.closest('form').submit();"><span class="mail-logo"></span>Logout</a>
             </form>
 
-            </li>
+        </li>
 
-            <li>
+        {{-- <li>
 
-              <div class="dropdown">
+            <div class="dropdown">
 
                 <button class="btn dropdown-toggle profile-dropdown" type="button" id="dropdownMenuButton"
+                    data-toggle="dropdown" aria-expanded="false">
 
-                  data-toggle="dropdown" aria-expanded="false">
+                    <div class="loggedin-user">
 
-                  <div class="loggedin-user">
+                        <img src="{{ asset('frontend/img/profile-avatar.png') }}" alt="avatar" />
 
-                    <img src="{{asset('frontend/img/profile-avatar.png')}}" alt="avatar" />
-
-                  </div>
+                    </div>
 
                 </button>
 
                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
 
-                  <a class="dropdown-item" href="{{ route('updateprofile.create')}}">Profile</a>
+                    <a class="dropdown-item" href="{{ route('updateprofile.create') }}">Profile</a>
 
-                  <a class="dropdown-item" href="#">My Account</a>
+                    <a class="dropdown-item" href="#">My Account</a>
 
-                  <form method="POST" action="{{ route('logout') }}">
+                    <form method="POST" action="{{ route('logout') }}">
 
-            @csrf
+                        @csrf
 
-            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                     this.closest('form').submit();">Logout</a>
-          </form>
+                    </form>
 
                 </div>
 
-              </div>
+            </div>
 
-            </li>
+        </li> --}}
 
-          </ul>
+    </ul>
 
 
 
-        </div>
-
+</div>
