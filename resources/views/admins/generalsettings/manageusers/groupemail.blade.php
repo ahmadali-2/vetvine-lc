01dfb23cb828411e6d86f9c65245fc96d0f230a1
mirzawajidali <wajidm341@gmail.com>
2022-07-15 09:37:46 -0700

    validation fixed

--- resources/views/admins/generalsettings/manageusers/groupemail.blade.php
+++ resources/views/admins/generalsettings/manageusers/groupemail.blade.php
@@ -1,29 +1,37 @@
 @extends('admins.master')
 @section('content')
-<section class="content-header">
-         <div class="container-fluid">
-             <div class="row mb-2">
-                 <div class="col-sm-6">
-                 </div>
-                 <div class="col-sm-6">
-                     {{-- <a class="btn btn-primary float-right" href="">
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                </div>
+                <div class="col-sm-6">
+                    {{-- <a class="btn btn-primary float-right" href="">
                        Group Mail
                      </a> --}}
-                 </div>
-             </div>
-         </div>
-     </section>
-     <form id="sendMailForm" action="{{ route('group.mail.sent') }}" method="post">
+                </div>
+            </div>
+        </div>
+    </section>
+    <form id="sendMailForm" action="{{ route('group.mail.sent') }}" method="post">
         @csrf
-     <div class="animated fadeIn">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="card">
-                    <div class="card-header">
-                        <strong class="card-title"> Mail to Members</strong>
-                    </div>
-                    <div class="card-body">
-
+        <div class="animated fadeIn">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <strong class="card-title"> Mail to Members</strong>
+                        </div>
+                        <div class="card-body">
+                            @if ($errors->any())
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
                             <div class="form-group">
                                 <input type="text" name="subject" placeholder="Subject" class="form-control">
                             </div>
@@ -31,23 +39,23 @@
                                 <textarea name="message" class="form-control" placeholder="Message" id="" cols="30" rows="3"></textarea>
                             </div>
 
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-            <div class="animated fadeIn">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="card">
-                            {{-- <div class="card-header">
+        <div class="animated fadeIn">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        {{-- <div class="card-header">
                                 <strong class="card-title"> Mail to Members</strong>
                             </div> --}}
-                            <div class="card-body">
+                        <div class="card-body">
 
-                                <table class="table" id="example">
-                                    <thead>
-                                      <tr>
+                            <table class="table" id="example">
+                                <thead>
+                                    <tr>
                                         <th scope="col">
                                             <div>
                                                 <input type="checkbox" class="checkAll">
@@ -59,62 +67,63 @@
                                         <th scope="col">Network</th>
                                         <th scope="col">User Type</th>
 
-                                      </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($allUsers as $user)
-                                      <tr>
-                                        <th scope="row">
-                                            <div>
-                                                <input type="checkbox" name="user[]" value="{{ $user->id }}">
-                                            </div>
-                                        </th>
-                                        <td>{{ $user->name }}</td>
-                                        <td>{{ $user->email }}</td>
-                                        <td>{{$user->userNetworkLevel->name}}</td>
-                                        <td>{{$user->userMemberType->member_type}}</td>
-                                      </tr>
-                                      @endforeach
-                                    </tbody>
-                                    <tfoot>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($allUsers as $user)
                                         <tr>
-                                            <td>
+                                            <th scope="row">
                                                 <div>
-                                                    <button type="submit" class="btn btn-primary">Send Mail</button>
+                                                    <input type="checkbox" name="user[]" value="{{ $user->id }}">
                                                 </div>
-                                            </td>
+                                            </th>
+                                            <td>{{ $user->name }}</td>
+                                            <td>{{ $user->email }}</td>
+                                            <td>{{ $user->userNetworkLevel->name }}</td>
+                                            <td>{{ $user->userMemberType->member_type }}</td>
                                         </tr>
-                                    </tfoot>
-                                  </table>
-                            </div>
+                                    @endforeach
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <td>
+                                            <div>
+                                                <button type="submit" class="btn btn-primary">Send Mail</button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tfoot>
+                            </table>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
     </form>
 @endsection
 
 @section('scripts')
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script>
-       $(document).ready(function(){
-        $(".checkAll").on('change',function(){
-            $('input:checkbox').prop('checked',$(this).prop("checked"));
+        $(document).ready(function() {
+            $(".checkAll").on('change', function() {
+                $('input:checkbox').prop('checked', $(this).prop("checked"));
+            });
+
+            // $('#sendMailForm').on('submit',function(e){
+            //     e.preventDefault();
+            //     $.ajax({
+            //         url: '{{ route('group.mail.sent') }}',
+            //         type: 'post',
+            //         data:  $(this).serialize(),
+            //         success: function(res){
+            //             console.log(res);
+            //         }
+            //     });
+            // });
+
+
         });
-
-        // $('#sendMailForm').on('submit',function(e){
-        //     e.preventDefault();
-        //     $.ajax({
-        //         url: '{{ route('group.mail.sent') }}',
-        //         type: 'post',
-        //         data:  $(this).serialize(),
-        //         success: function(res){
-        //             console.log(res);
-        //         }
-        //     });
-        // });
-
-
-       });
     </script>
 @endsection
