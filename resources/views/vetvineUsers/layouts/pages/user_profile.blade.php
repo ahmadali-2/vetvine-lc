11231c183af00635c418448d0a4da35f3e10e384
ahmadali-2 <ahmadali.2@yahoo.com>
2022-07-28 08:24:33 -0700

    Changes by awais designer

--- resources/views/vetvineUsers/layouts/pages/user_profile.blade.php
+++ resources/views/vetvineUsers/layouts/pages/user_profile.blade.php
@@ -112,7 +112,7 @@
                                         </div>
                                         <div class="serch-section">
 
-                                            <div class="inner-input license-field @if(Auth::user()->network_id===6 || Auth::user()->network_id===7) show @endif" id="license-div">
+                                            <div class="inner-input license-field d-flex @if(Auth::user()->network_id===6 || Auth::user()->network_id===7) show @endif" id="license-div">
                                                 <label>Licensure*</label>
                                                 <div class="input_field" >
                                                     <input placeholder="" class="form-control license-inp" name="licensure" @if(Auth::user()->network_id===6 || Auth::user()->network_id===7)
