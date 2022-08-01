65fc0d32fae6e4dd99a46f8ddb2c3cafb154f580
mirzawajidali <wajidm341@gmail.com>
2022-08-01 09:22:11 -0700

    Changes by Wajid 8/1/2022

--- resources/views/admins/layouts/_head.blade.php
+++ resources/views/admins/layouts/_head.blade.php
@@ -1,50 +1,68 @@
    <meta charset="utf-8">
 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title>vetvine- Dashboard</title>
+   <title>vetvine- Dashboard</title>
 
-    <meta name="description" content="Sufee Admin - HTML5 Admin Template">
+   <meta name="description" content="Sufee Admin - HTML5 Admin Template">
 
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-
-    <link rel="apple-touch-icon" href="apple-icon.png">
-
-    <link rel="shortcut icon" href="favicon.ico">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
 
-    <link rel="stylesheet" href="{{ asset('admin/vendors/bootstrap/dist/css/bootstrap.min.css ')}}">
+   <link rel="apple-touch-icon" href="apple-icon.png">
 
-    <link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css" rel="stylesheet">
+   <link rel="shortcut icon" href="favicon.ico">
+
+   {{-- <link rel="stylesheet"
+       href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.css"
+       integrity="sha512-xmGTNt20S0t62wHLmQec2DauG9T+owP9e6VU8GigI0anN7OXLip9i7IwEhelasml2osdxX71XcYm6BQunTQeQg=="
+       crossorigin="anonymous" /> --}}
+
+   <link rel="stylesheet" href="{{ asset('admin/vendors/bootstrap/dist/css/bootstrap.min.css ') }}">
+
+   <link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css"
+       rel="stylesheet">
 
 
 
-    <link rel="stylesheet" href="{{ asset('admin/vendors/font-awesome/css/font-awesome.min.css')}}">
+   <link rel="stylesheet" href="{{ asset('admin/vendors/font-awesome/css/font-awesome.min.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('admin/vendors/themify-icons/css/themify-icons.css')}}">
+   <link rel="stylesheet" href="{{ asset('admin/vendors/themify-icons/css/themify-icons.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('admin/vendors/flag-icon-css/css/flag-icon.min.css')}}">
+   <link rel="stylesheet" href="{{ asset('admin/vendors/flag-icon-css/css/flag-icon.min.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('admin/vendors/selectFX/css/cs-skin-elastic.css')}}">
+   <link rel="stylesheet" href="{{ asset('admin/vendors/selectFX/css/cs-skin-elastic.css') }}">
 
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ asset('vendor/select2/dist/css/select2.min.css') }}" />
+   <link rel="stylesheet" href="{{ asset('vendor/select2/dist/css/select2.min.css') }}" />
 
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css')}}">
+   <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}">
 
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+   <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+   <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
 
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
+   <meta name="csrf-token" content="{{ csrf_token() }}" />
+   <style>
+       .bootstrap-tagsinput {
+           background: none !important;
+           border: 1px solid #ced4da !important;
+           border-radius: 3px !important;
+           padding: 5px !important;
+       }
 
-    @toastr_css
+       .tag {
+           background: #f27222 !important;
+           border-radius: 3px !important;
+           padding: 3px 6px !important;
+           color: #fff !important;
+       }
+   </style>
 
-    @notifyCss
+   @toastr_css
 
+   @notifyCss
